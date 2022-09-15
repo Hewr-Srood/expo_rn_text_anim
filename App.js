291c4f9b0c051947f2e0df5363df987fbb8b7fa1
Hewr Srood <hewr_s@outlook.com>
2022-09-15 14:22:15 +0300

    expo v

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StyleSheet } from "react-native";
+import {
+  Canvas,
+  Skia,
+  useFont,
+  useValue,
+  useSharedValueEffect,
+  mix,
+  Path,
+  Fill,
+} from "@shopify/react-native-skia";
+import {
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
+import Font from "./my-font.ttf";
+export default () => {
+  const font = useFont(Font, 64);
 
-export default function App() {
+  const progress = useSharedValue(0);
+  const end = useValue(0);
+
+  useEffect(() => {
+    progress.value = withRepeat(withTiming(2, { duration: 5000 }), -1, true);
+  }, [progress]);
+
+  useSharedValueEffect(() => {
+    end.current = mix(progress.value, 0, 1);
+  }, progress);
+
+  if (font === null) {
+    return null;
+  }
+  const path = Skia.Path.MakeFromText("Hello World", 1, 100, font);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Canvas style={styles.canvas}>
+      <Fill color={"#fff"} />
+      <Path
+        style="stroke"
+        strokeJoin="round"
+        strokeWidth={1}
+        path={path}
+        color="#3C91FC"
+        start={0}
+        end={end}
+      />
+    </Canvas>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  canvas: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
